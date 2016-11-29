50122d4409330a0622dd9d0d4c752a6df918f623
sdist <sdist@itam.mx>
2016-11-29 12:37:52 -0600

    Google maps app

---
+++ GoogleMapsApp/app/src/main/java/worrengilgamesh/googlemapsapp/MainActivity.java
@@ -0,0 +1,71 @@
+package worrengilgamesh.googlemapsapp;
+
+import android.app.Dialog;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Toast;
+
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.GoogleApiAvailability;
+import com.google.android.gms.maps.CameraUpdate;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapFragment;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.model.LatLng;
+
+import java.util.Map;
+
+public class MainActivity extends AppCompatActivity implements OnMapReadyCallback {
+
+    GoogleMap mGoogleMap;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (googleServicesAvaible()){
+            Toast.makeText(this,"Perfect", Toast.LENGTH_LONG).show();
+            setContentView(R.layout.activity_main);
+            initMap();
+        }else {
+            //No Google Maps LayOut
+        }
+    }
+
+    private void initMap() {
+        MapFragment mapFragment =(MapFragment) getFragmentManager().findFragmentById(R.id.mapFragment);
+        mapFragment.getMapAsync(this);
+    }
+
+    public boolean googleServicesAvaible(){
+        GoogleApiAvailability api =GoogleApiAvailability.getInstance();
+        int isAvailable =api.isGooglePlayServicesAvailable(this);
+        if (isAvailable== ConnectionResult.SUCCESS){
+            return true;
+        }else if (api.isUserResolvableError(isAvailable)){
+            Dialog dialog = api.getErrorDialog(this,isAvailable,0);
+            dialog.show();
+        }else{
+            Toast.makeText(this,"Cant play seriveces", Toast.LENGTH_LONG).show();
+        }
+        return false;
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mGoogleMap = googleMap;
+        goToLocationZoom(19.344808,-99.2000187, 15);
+    }
+
+    private void goToLocation(double lat, double ing) {
+        LatLng ll = new LatLng(lat, ing);
+        CameraUpdate update = CameraUpdateFactory.newLatLng(ll);
+        mGoogleMap.moveCamera(update);
+    }
+
+    private void goToLocationZoom(double lat, double ing, int zoom) {
+        LatLng ll = new LatLng(lat, ing);
+        CameraUpdate update = CameraUpdateFactory.newLatLngZoom(ll, zoom);
+        mGoogleMap.moveCamera(update);
+    }
+}
